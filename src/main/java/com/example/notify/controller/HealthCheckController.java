69600fb6b67be9fd7eae1b16abc24debe756206b
ashi0405 <aashitabajaj2@gmail.com>
2025-04-30 00:07:19 +0530

    Added health controller for Railway deployment

--- src/main/java/com/example/notify/controller/HealthCheckController.java
+++ src/main/java/com/example/notify/controller/HealthCheckController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthCheckController {
-    @GetMapping("/health")
-    public ResponseEntity<String> health() {
-        return ResponseEntity.ok("OK");
+    @GetMapping("/")
+    public String healthCheck() {
+        return "Notification Service is running";
     }
 }
