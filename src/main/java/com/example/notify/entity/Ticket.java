18067f82f307bda63290e17073147825dd2b2a7b
ashi0405 <aashitabajaj2@gmail.com>
2025-04-29 22:57:41 +0530

    Added health controller for Railway deployment

--- src/main/java/com/example/notify/entity/Ticket.java
+++ src/main/java/com/example/notify/entity/Ticket.java
@@ -8,9 +8,9 @@ import lombok.Setter;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-@Entity
 @Getter
 @Setter
+@Entity
 public class Ticket {
 
     @Id
@@ -29,6 +29,42 @@ public class Ticket {
     private LocalDate journeyDate;
 
     private LocalDateTime lastCheckedAt;
+
+    // existing fields
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getLastCheckedAt() {
+        return lastCheckedAt;
+    }
+
+    public void setLastCheckedAt(LocalDateTime lastCheckedAt) {
+        this.lastCheckedAt = lastCheckedAt;
+    }
+
+// existing fields
+
+    public LocalDate getJourneyDate() {
+        return journeyDate;
+    }
+
+    public void setJourneyDate(LocalDate journeyDate) {
+        this.journeyDate = journeyDate;
+    }
+
+    public String getPnrNumber() {
+        return pnrNumber;
+    }
+
+    public void setPnrNumber(String pnrNumber) {
+        this.pnrNumber = pnrNumber;
+    }
 
 
 }
